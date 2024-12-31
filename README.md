3cfdaf36fb01d1ca5bd16e6523fe5e2a1b734722
zlj <zlj@zljdeMacBook-Pro.local>
2024-12-31 20:59:13 +0800

    feat: add iterm2

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # dev
 Setting up development environment.
 
+## Install iterm2
+```bash
+cd iterm2
+sh install.sh
+```
+
 ## Install tmux
 ```bash
 cd tmux
---
+++ iterm2/install.sh
@@ -0,0 +1,6 @@
+echo "install iterm2"
+brew install --cask iterm2
+
+echo "install nerd fonts"
+# https://github.com/ryanoasis/nerd-fonts
+brew install font-hack-nerd-font
