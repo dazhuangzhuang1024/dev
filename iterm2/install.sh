1316391a072d760253f708840997574bf7c03cc3
zlj <zlj@zljdeMacBook-Pro.local>
2025-01-02 17:27:05 +0800

    feat: add fzf in iterm2 install script

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,6 +1,10 @@
-echo "install iterm2"
+echo "install iterm2 ..."
 brew install --cask iterm2
 
-echo "install nerd fonts"
+echo "install nerd fonts ..."
 # https://github.com/ryanoasis/nerd-fonts
 brew install font-hack-nerd-font
+
+echo "install fzf ..."
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
